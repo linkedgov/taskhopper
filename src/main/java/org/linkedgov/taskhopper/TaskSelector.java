de367e4ad1fcc79b5cdaf66a58743ab93600b3ab
Tom Morris <tom@tommorris.org>
2011-08-30 13:46:33 +0100

    rewritten code to use XOM

--- src/main/java/org/linkedgov/taskhopper/TaskSelector.java
+++ src/main/java/org/linkedgov/taskhopper/TaskSelector.java
@@ -1,17 +1,15 @@
 package org.linkedgov.taskhopper;
 import java.io.IOException;
 import java.io.InputStream;
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
+import nu.xom.ParsingException;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
-import org.linkedgov.taskhopper.support.NullResolver;
-import org.w3c.dom.Document;
 import org.xml.sax.SAXException;
+import nu.xom.Builder;
+import nu.xom.Document;
 
 public class TaskSelector {
 
@@ -25,56 +23,47 @@ public class TaskSelector {
         this.client = new DefaultHttpClient();
     }
 
-    public Document byId(int taskId) throws IOException, ParserConfigurationException, SAXException {
+    public Document byId(int taskId) throws IOException, SAXException, ParsingException {
         Document xml = this.loadDocument("get.xq?id=" + Integer.toString(taskId));
         return xml;
     }
 
-    public Document random() throws IOException, ParserConfigurationException, SAXException {
+    public Document random() throws IOException, SAXException, ParsingException {
         Document xml = this.loadDocument("get_random.xq");
         return xml;
     }
 
-    public Document randomByType(String type) throws IOException, ParserConfigurationException, SAXException {
+    public Document randomByType(String type) throws IOException, SAXException, ParsingException {
         Document xml = this.loadDocument("random_by_type.xq?type=" + type);
         return xml;
     }
 
-    public Document all() throws IOException, ParserConfigurationException, SAXException {
+    public Document all() throws IOException, SAXException, ParsingException {
         Document xml = this.loadDocument("get.xq");
         return xml;
     }
 
-    public Document priority() throws IOException, ParserConfigurationException, SAXException {
+    public Document priority() throws IOException, SAXException, ParsingException {
         Document xml = this.loadDocument("priority.xq");
         return xml;
     }
 
-//    public void create() throws IOException, ParserConfigurationException, SAXException {
+//    public void create() throws IOException, SAXException {
 //
 //    }
     
-    private Document loadDocument(String url) throws IOException, ParserConfigurationException, SAXException {
+    private nu.xom.Document loadDocument(String url) throws IOException, SAXException, ParsingException {
         HttpGet get = new HttpGet("http://" + this.address + ":" + this.port +
                 "/exist/rest/db/linkedgov-meta/taskhopper/" + url);
         HttpResponse response = this.client.execute(get);
         HttpEntity entity = response.getEntity();
-        Document xml = TaskSelector.readDocument(entity.getContent());
+        Document xml = TaskSelector.readDocumentXom(entity.getContent());
         return xml;
     }
 
-    private static Document readDocument(InputStream is)
-            throws ParserConfigurationException, SAXException, IOException {
-        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-        dbf.setValidating(false);
-        dbf.setIgnoringComments(false);
-        dbf.setIgnoringElementContentWhitespace(true);
-        dbf.setNamespaceAware(true);
-
-        DocumentBuilder db = null;
-        db = dbf.newDocumentBuilder();
-        db.setEntityResolver(new NullResolver());
-
-        return db.parse(is);
+    private static nu.xom.Document readDocumentXom(InputStream is) throws ParsingException, IOException {
+        Builder parser = new Builder();
+        nu.xom.Document doc = parser.build(is);
+        return doc;
     }
 }

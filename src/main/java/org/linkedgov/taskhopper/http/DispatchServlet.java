42ff2fbb3d76b3013a6a1eaa162018ce858ddf55
Tom Morris <tom@tommorris.org>
2011-09-01 14:27:37 +0100

    rm unused RequestDispatcher from DispatchServlet

--- src/main/java/org/linkedgov/taskhopper/http/DispatchServlet.java
+++ src/main/java/org/linkedgov/taskhopper/http/DispatchServlet.java
@@ -2,7 +2,6 @@ package org.linkedgov.taskhopper.http;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;

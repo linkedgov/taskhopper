fa8535668c76771052995255a807c3729d1cbf43
Tom Morris <tom@tommorris.org>
2011-09-05 14:38:33 +0100

    TaskUpdater refactor common code > private statics
    getMainGraphFromDocument
    removePotentiallyIncorrect
    
    added JavaDocs for the two static methods

--- src/main/java/org/linkedgov/taskhopper/TaskUpdater.java
+++ src/main/java/org/linkedgov/taskhopper/TaskUpdater.java
@@ -10,7 +10,9 @@ import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.io.UnsupportedEncodingException;
+import java.util.Map;
 import nu.xom.Builder;
 import nu.xom.Document;
 import nu.xom.Element;
@@ -37,6 +39,21 @@ public class TaskUpdater {
     public TaskUpdater(Connection conn) {
         this.connection = conn;
     }
+
+    /* TODO: method for modifying one of the issues (by issue URI) and then
+     * modifying the graph */
+    public void update(Task task, Map<String, String> values) {
+        if (values.containsKey("null") && values.get("null").equals("true")) {
+            // we need to update the graph to reflect a null value
+        }
+    }
+
+    public void updateById(String id) {
+    }
+
+    public void merge() {
+    }
+    /* TODO: servlet for serving the above */
 
     /**
      * Method to modify document to mark a value from a task as null.
@@ -66,26 +83,10 @@ public class TaskUpdater {
         /* Now modify the RDF graph to remove the statement of the form
          * <subject> <potentiallyIncorrect> <issueId> . */
         /* First, we read in the RDF */
-        Element mainDoc = root.getFirstChildElement("main");
-        Element mainDocRDF = mainDoc.getFirstChildElement("RDF",
-                "http://www.w3.org/1999/02/22-rdf-syntax-ns#");
-        InputStream mainDocStream = new ByteArrayInputStream(
-                mainDocRDF.toXML().getBytes("UTF-8"));
-        Model model = ModelFactory.createDefaultModel();
-        model.read(mainDocStream, "");
-
-        /* Now select "potentiallyIncorrect" property. */
-        Property incorrect = model.createProperty(TaskSelector.potentiallyIncorrectUri);
-        Resource taskIdResource = model.createResource(taskId);
-        StmtIterator stmts = model.listStatements((Resource) null,
-                incorrect,
-                (Resource) taskIdResource);
+        Model model = TaskUpdater.getMainGraphFromDocument(document);
 
         /* Remove potentiallyIncorrect statements from graph. */
-        while (stmts.hasNext()) {
-            Statement stmt = (Statement) stmts.next();
-            model.remove(stmt);
-        }
+        model = TaskUpdater.removePotentiallyIncorrect(model, taskId);
 
         /* I/O jiggerypokery to move data from Jena back to XML document. */
         ByteArrayOutputStream rdfOutStream = new ByteArrayOutputStream();
@@ -98,10 +99,54 @@ public class TaskUpdater {
 
         /* Merge the RDF graph back into document in place of the original main
          * element. */
-        mainDoc.removeChildren();
-        mainDoc.insertChild(rdfOut.getRootElement().copy(), 0);
+        root.getFirstChildElement("main").removeChildren();
+        root.getFirstChildElement("main").insertChild(rdfOut.getRootElement().copy(), 0);
 
         /* Dump the whole document out. */
         return document;
+    }
+
+    /**
+     * Takes a linkedgov document and returns the main graph from it as a Jena
+     * model.
+     *
+     * @param document
+     * @throws UnsupportedEncodingException
+     * @return RDF model.
+     */
+    private static Model getMainGraphFromDocument(Document document)
+            throws UnsupportedEncodingException {
+        Element root = document.getRootElement();
+        Element mainDoc = root.getFirstChildElement("main");
+        Element mainDocRDF = mainDoc.getFirstChildElement("RDF",
+                "http://www.w3.org/1999/02/22-rdf-syntax-ns#");
+        InputStream mainDocStream = new ByteArrayInputStream(
+                mainDocRDF.toXML().getBytes("UTF-8"));
+        Model model = ModelFactory.createDefaultModel();
+        model.read(mainDocStream, "");
+        return model;
+    }
+
+    /**
+     * Removes a potentially-incorrect attribute from the main model.
+     *
+     * @param model
+     * @param taskId
+     * @return
+     */
+    private static Model removePotentiallyIncorrect(Model model, String taskId) {
+        /* Now select "potentiallyIncorrect" property. */
+        Property incorrect = model.createProperty(TaskSelector.potentiallyIncorrectUri);
+        Resource taskIdResource = model.createResource(taskId);
+        StmtIterator stmts = model.listStatements((Resource) null,
+                incorrect,
+                (Resource) taskIdResource);
+
+        /* Remove potentiallyIncorrect statements from graph. */
+        while (stmts.hasNext()) {
+            Statement stmt = (Statement) stmts.next();
+            model.remove(stmt);
+        }
+        return model;
     }
 }

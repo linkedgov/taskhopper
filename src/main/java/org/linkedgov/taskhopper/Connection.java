cd81f7df779b078416c6f2760111f8fe48858293
Tom Morris <tom@tommorris.org>
2011-09-05 12:07:00 +0100

    added authentication to Connection

--- src/main/java/org/linkedgov/taskhopper/Connection.java
+++ src/main/java/org/linkedgov/taskhopper/Connection.java
@@ -12,6 +12,8 @@ import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
 import org.xml.sax.SAXException;
 
 public class Connection {
@@ -48,8 +50,9 @@ public class Connection {
     public void setPort(Integer port) {
         this.port = port;
     }// </editor-fold>
-    // <editor-fold defaultstate="collapsed" desc="HttpClient client;">
-    private HttpClient client;
+    // <editor-fold defaultstate="collapsed" desc="DefaultHttpClient client;">
+    private DefaultHttpClient client;
+
     /**
      * @return the client
      */
@@ -60,14 +63,77 @@ public class Connection {
     /**
      * @param client the client to set
      */
-    public void setClient(HttpClient client) {
+    public void setClient(DefaultHttpClient client) {
         this.client = client;
     }//</editor-fold>
+    // <editor-fold defaultstate="collapsed" desc="String username;">
+    private String username;
+
+    /**
+     * @return the username
+     */
+    public String getUsername() {
+        return username;
+    }
+
+    /**
+     * @param username the username to set
+     */
+    public void setUsername(String username) {
+        this.username = username;
+        this.authenticationCallback();
+    } // </editor-fold>
+    // <editor-fold defaultstate="collapsed" desc="String password;">
+    private String password;
+
+    /**
+     * @return the password
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * @param password the password to set
+     */
+    public void setPassword(String password) {
+        this.password = password;
+        this.authenticationCallback();
+    }
+    // </editor-fold>
+    // <editor-fold defaultstate="collapsed" desc="boolean authenticating;">
+    private boolean authenticating;
+
+    /**
+     * @return the authenticating
+     */
+    public boolean isAuthenticating() {
+        return authenticating;
+    }
+
+    /**
+     * @param authenticating the authenticating to set
+     */
+    public void setAuthenticating(boolean authenticating) {
+        this.authenticating = authenticating;
+    } //</editor-fold>
+
+    private void authenticationCallback() {
+        if (this.username != null && this.password != null) {
+            this.client.getCredentialsProvider().setCredentials(
+                    new AuthScope(this.url, this.port),
+                    new UsernamePasswordCredentials(this.username, this.password));
+            this.setAuthenticating(true);
+        } else {
+            this.setAuthenticating(false);
+        }
+    }
 
     public Connection(String url, Integer port) {
         this.setUrl(url);
         this.setPort(port);
         this.client = new DefaultHttpClient();
+        this.setAuthenticating(false);
     }
 
     public Document loadUrl(String url) throws ParsingException, IOException {
@@ -77,6 +143,7 @@ public class Connection {
         Document xml = Connection.readDocument(entity.getContent());
         return xml;
     }
+
     /**
      * Loads a document from the database and parses it into an <code>Document</code>.
      *

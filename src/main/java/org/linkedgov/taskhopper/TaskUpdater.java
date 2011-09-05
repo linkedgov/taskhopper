eb3cda5c146c5927cd1d2fec351a091d0d8526e6
Tom Morris <tom@tommorris.org>
2011-09-05 15:48:08 +0100

    created editValue method in TaskUpdater
    editValue lets you move a literal from an task to a main without using
    SPARQL or XQuery (just low-level XML and RDF manipulation)
    
    Created two support methods in TaskUpdater:
    
    private static Element getTaskElementFromDocument
    private static Model getTaskGraphFromDocument

--- src/main/java/org/linkedgov/taskhopper/TaskUpdater.java
+++ src/main/java/org/linkedgov/taskhopper/TaskUpdater.java
@@ -68,11 +68,12 @@ public class TaskUpdater {
      */
     public static Document nullifyTask(Document document, String taskId)
             throws UnsupportedEncodingException, ParsingException,
-                ValidityException, IOException {
+            ValidityException, IOException {
         /* Nullifying a task consists of removing the task from the
          * XML document and removing the reference to the task from
          * the main RDF document. */
         Element root = document.getRootElement();
+        // TODO: switch to getTaskElementFromDocument.
         String taskQuery = String.format("//issue[@uri = '%s']", taskId);
         Nodes taskElems = root.query(taskQuery);
         for (int i = 0; i < taskElems.size(); i++) {
@@ -94,7 +95,7 @@ public class TaskUpdater {
         model.close();
 
         Builder builder = new Builder();
-        Document rdfOut = 
+        Document rdfOut =
                 builder.build(new ByteArrayInputStream(rdfOutStream.toByteArray()));
 
         /* Merge the RDF graph back into document in place of the original main
@@ -103,6 +104,62 @@ public class TaskUpdater {
         root.getFirstChildElement("main").insertChild(rdfOut.getRootElement().copy(), 0);
 
         /* Dump the whole document out. */
+        return document;
+    }
+
+    public static Document editValue(Document document, String taskId,
+            String replacementValue, String replacementXsdType)
+            throws UnsupportedEncodingException, ParsingException,
+            ValidityException, IOException {
+        /* In the code and comments below, I refer to two graphs:
+         * 
+         * 1. the document graph is what is contained in <main />
+         * 
+         * 2. the task graph is the named graph in a particular
+         *    <task />.
+         */
+        Element root = document.getRootElement();
+        Model model = TaskUpdater.getMainGraphFromDocument(document);
+        if (replacementXsdType != null) {
+            // TODO: implement something intelligent to do with replacement XSD types.
+        }
+
+        Model taskGraph = TaskUpdater.getTaskGraphFromDocument(document, taskId);
+        StmtIterator stmts = taskGraph.listStatements();
+        while (stmts.hasNext()) {
+            Statement stmt = (Statement) stmts.next();
+            /* changeObject mutates the graph! The word 'change' kind of
+             * hints at that, but it is still potentially surprising.
+             * Given what we are intending to do with the statement,
+             * this is an unnecessary side-effect, but we are going to
+             * be getting rid of the contents of the taskGraph anyway.
+             */
+            Statement newStmt = stmt.changeObject(replacementValue);
+            /* Add to the document graph rather than the task graph. */
+            model.add(newStmt);
+        }
+        /* We are done with the TaskGraph, so let's close it. */
+        taskGraph.close();
+        /* Remove the references from the document graph. */
+        TaskUpdater.removePotentiallyIncorrect(model, taskId);
+
+        /* Now update the XML: write us a new main element. */
+        ByteArrayOutputStream rdfOutStream = new ByteArrayOutputStream();
+        model.write(rdfOutStream, "RDF/XML-ABBREV");
+        model.close();
+
+        Builder builder = new Builder();
+        Document rdfOut =
+                builder.build(new ByteArrayInputStream(rdfOutStream.toByteArray()));
+
+        /* Merge the RDF graph back into document in place of the original main
+         * element. */
+        root.getFirstChildElement("main").removeChildren();
+        root.getFirstChildElement("main").insertChild(rdfOut.getRootElement().copy(), 0);
+
+        /* Finally, detach the task element from the document. */
+        TaskUpdater.getTaskElementFromDocument(document, taskId).getParent().detach();
+
         return document;
     }
 
@@ -148,5 +205,40 @@ public class TaskUpdater {
             model.remove(stmt);
         }
         return model;
+    }
+
+    /**
+     * Takes a document and returns a 'task graph' as a Jena model.
+     *
+     * @param document
+     * @param taskId
+     * @return
+     * @throws UnsupportedEncodingException
+     */
+    private static Model getTaskGraphFromDocument(Document document, String taskId) throws UnsupportedEncodingException {
+        Element taskRDF = TaskUpdater.getTaskElementFromDocument(document, taskId);
+        if (taskRDF != null) {
+            InputStream mainDocStream = new ByteArrayInputStream(
+                    taskRDF.toXML().getBytes("UTF-8"));
+            Model model = ModelFactory.createDefaultModel();
+            model.read(mainDocStream, "");
+            return model;
+        } else {
+            return null;
+        }
+    }
+
+    private static Element getTaskElementFromDocument(Document document, String taskId) {
+        Element root = document.getRootElement();
+        String taskQuery = String.format("//issue[@uri = '%s']", taskId);
+        Nodes taskElems = root.query(taskQuery);
+        if (taskElems.size() == 0) {
+            return null;
+        } else {
+            Element task = (Element) taskElems.get(0);
+            Element taskRDF = task.getFirstChildElement("RDF",
+                    "http://www.w3.org/1999/02/22-rdf-syntax-ns#");
+            return taskRDF;
+        }
     }
 }

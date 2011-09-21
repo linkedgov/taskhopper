52ce7f6f3584b259fb99d062fca5cef5975b1275
Tom Morris <tom@tommorris.org>
2011-09-21 12:22:24 +0100

    RDF/XML to RDF/JSON converter
    Took the RDF/JSON writer from Talis, rewrote to emit Jettison JSONArray objects rather than to dump strings into Writers.
    
    Written a simple unit test to test the RDF/JSON functionality

---
+++ src/main/java/org/linkedgov/taskhopper/support/RDFToJSON.java
@@ -0,0 +1,92 @@
+package org.linkedgov.taskhopper.support;
+
+import com.hp.hpl.jena.rdf.model.Literal;
+import com.hp.hpl.jena.rdf.model.Model;
+import com.hp.hpl.jena.rdf.model.ModelFactory;
+import com.hp.hpl.jena.rdf.model.Property;
+import com.hp.hpl.jena.rdf.model.ResIterator;
+import com.hp.hpl.jena.rdf.model.Resource;
+import com.hp.hpl.jena.rdf.model.Statement;
+import com.hp.hpl.jena.rdf.model.StmtIterator;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashSet;
+import org.codehaus.jettison.json.JSONException;
+import org.codehaus.jettison.json.JSONObject;
+import org.codehaus.jettison.json.JSONArray;
+import nu.xom.Element;
+
+public class RDFToJSON {
+
+    public static JSONObject rdfXmlToJson(Element xml) throws IOException, JSONException {
+        Model model = ModelFactory.createDefaultModel();
+        InputStream mainDocStream = new ByteArrayInputStream(
+                xml.toXML().getBytes("UTF-8"));
+        model.read(mainDocStream, "");
+        JSONObject json = new JSONObject();
+        ResIterator subjectIterator = model.listSubjects();
+        while (subjectIterator.hasNext()) {
+            Resource subjectResource = subjectIterator.nextResource();
+            String key = "";
+            if (subjectResource.isAnon()) {
+                key = "_:" + subjectResource.asNode().getBlankNodeId().getLabelString();
+            } else {
+                key = subjectResource.getURI();
+            }
+            json.put(key, processProperties(subjectResource));
+        }
+        return json;
+    }
+
+    private static JSONObject processProperties(Resource subjectResource)
+            throws IOException, JSONException {
+        JSONObject out = new JSONObject();
+
+        HashSet<Property> propertiesToProcess = new HashSet<Property>();
+        StmtIterator allPropertiesIterator = subjectResource.listProperties();
+        while (allPropertiesIterator.hasNext()) {
+            Statement statement = allPropertiesIterator.nextStatement();
+            if (!propertiesToProcess.contains(statement.getPredicate())) {
+                propertiesToProcess.add(statement.getPredicate());
+            }
+        }
+        for (Property property : propertiesToProcess) {
+            String key = property.getURI();
+            StmtIterator propertyIterator = subjectResource.listProperties(property);
+            JSONArray propList = new JSONArray();
+            while (propertyIterator.hasNext()) {
+                Statement propertyStatement = propertyIterator.nextStatement();
+                propList.put(processProperty(propertyStatement));
+            }
+            out.put(key, propList);
+        }
+
+        return out;
+    }
+
+    private static JSONObject processProperty(Statement property) throws IOException, JSONException {
+        JSONObject out = new JSONObject();
+        if (property.getObject().isURIResource()) {
+            Resource r = (Resource) property.getObject();
+            out.put("value", r.getURI());
+            out.put("type", "uri");
+        } else if (property.getObject().isLiteral()) {
+            Literal l = (Literal) property.getObject();
+            out.put("value", l.getLexicalForm());
+            out.put("type", "literal");
+            String languageValue = l.getLanguage();
+            if (languageValue != null && !languageValue.trim().equals("")) {
+                out.put("lang", languageValue);
+            }
+            String dataTypeValue = l.getDatatypeURI();
+            if (dataTypeValue != null && !dataTypeValue.trim().equals("")) {
+                out.put("datatype", dataTypeValue);
+            }
+        } else if (property.getObject().isAnon()) {
+            out.put("value", property.getObject().asNode().getBlankNodeId().getLabelString());
+            out.put("type", "bnode");
+        }
+        return out;
+    }
+}
---
+++ src/test/java/org/linkedgov/taskhopper/support/RDFToJSONTest.java
@@ -0,0 +1,58 @@
+package org.linkedgov.taskhopper.support;
+
+import junit.framework.TestCase;
+import nu.xom.Builder;
+import nu.xom.Document;
+import nu.xom.Element;
+import org.codehaus.jettison.json.JSONArray;
+import org.codehaus.jettison.json.JSONObject;
+
+public class RDFToJSONTest extends TestCase {
+    
+    public RDFToJSONTest(String testName) {
+        super(testName);
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
+
+    /**
+     * Test of rdfXmlToJson method, of class RDFToJSON.
+     */
+    public void testRdfXmlToJson() throws Exception {
+        System.out.println("rdfXmlToJson");
+        // define XML input
+        String rdfEx = "<?xml version=\"1.0\" ?>\n" +
+                "<rdf:RDF xmlns:rdf=\"http://www.w3.org/1999/02/22-rdf-syntax-ns#\"\n" +
+                " xmlns:dc=\"http://purl.org/dc/elements/1.1/\">\n" +
+                "<rdf:Description rdf:about=\"http://example.org/1\">\n" +
+                "<dc:title>Example resource</dc:title>\n" +
+                "</rdf:Description>\n" +
+                "</rdf:RDF>\n";
+        // parse into XOM document
+        Builder builder = new Builder();
+        Document doc = builder.build(rdfEx, "");
+        Element xml = doc.getRootElement();
+        // defining the output of the parser.
+        JSONObject expValue = new JSONObject();
+        expValue.put("value", "Example resource");
+        expValue.put("type", "literal");
+        JSONArray expValueArray = new JSONArray();
+        expValueArray.put(expValue);
+        JSONObject expProperty = new JSONObject();
+        expProperty.put("http://purl.org/dc/elements/1.1/title", expValueArray);
+        JSONObject expResult = new JSONObject();
+        expResult.put("http://example.org/1", expProperty);
+        // do transform, check result
+        JSONObject result = RDFToJSON.rdfXmlToJson(xml);
+        assertEquals(expResult.toString(), result.toString());
+    }
+
+}

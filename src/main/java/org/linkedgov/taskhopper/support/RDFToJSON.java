571eb3fccb2a81686c152c10524300b022b56b3e
Tom Morris <tom@tommorris.org>
2011-10-07 12:33:52 +0100

    documented RDFToJSON

--- src/main/java/org/linkedgov/taskhopper/support/RDFToJSON.java
+++ src/main/java/org/linkedgov/taskhopper/support/RDFToJSON.java
@@ -19,35 +19,97 @@ import nu.xom.Element;
 /**
  * Convenience class for turning RDF/XML (XOM) to RDF/JSON (Jettison).
  *
+ * The logic of this class has been derived heavily from JSONJenaWriter, a Jena
+ * Writer. This was created as we don't want a "writer", we want to turn a Jena
+ * model into a Jettison JSONObject.
+ *
  * @author tom
  */
 public class RDFToJSON {
 
+    /* In RDF JSON, each subject is a key, which has as a value a JSON Object
+     * representing the property. Each property has an array, which has an
+     * object for each object that matches the same subject and predicate.
+     * That obejct is made up of type, value, lang and datatype.
+     *
+     * For instance:
+     *  @prefix : <http://example.org/>
+     *  :jane :age "30" .
+     *  :john :knows :jane.
+     *
+     * Would be turned into the following JSON:
+     *
+     * {
+     *       "http://example.org/jane": {
+     *          "http://example.org/jane": [
+     *              {
+     *                  "type": "literal",
+     *                  "value": "30"
+     *              }
+     *          ]
+     *      },
+     *      "http://example.org/john": {
+     *          "http://example.org/knows": [
+     *              {
+     *                  "type": "uri",
+     *                  "value": "http://example.org/jane"
+     *              }
+     *          ]
+     *      }
+     *  }
+     *
+     */
+    
+    /**
+     * Convert XOM Element containing RDF/XML to Jettison JSONObject containing RDF/JSON.
+     *
+     * @param xml XOM Element containing RDF/XML
+     * @return Jettison JSONObject
+     * @throws IOException
+     * @throws JSONException
+     */
     public static JSONObject rdfXmlToJson(Element xml) throws IOException, JSONException {
         Model model = ModelFactory.createDefaultModel();
         InputStream mainDocStream = new ByteArrayInputStream(
                 xml.toXML().getBytes("UTF-8"));
         model.read(mainDocStream, "");
         JSONObject json = new JSONObject();
+
+        /* Iterate through every subject in the graph. */
         ResIterator subjectIterator = model.listSubjects();
         while (subjectIterator.hasNext()) {
             Resource subjectResource = subjectIterator.nextResource();
             String key = "";
+            /* If the subject is a blank node (aka. anonymous node), we give it
+             * the BNodeID label string that Jena has given it. */
             if (subjectResource.isAnon()) {
                 key = "_:" + subjectResource.asNode().getBlankNodeId().getLabelString();
             } else {
+                /* Otherwise we use the URI. */
                 key = subjectResource.getURI();
             }
+            /* Now we get the properties affiliated with the subject. */
             json.put(key, processProperties(subjectResource));
         }
         return json;
     }
 
+    /**
+     * Convert RDF resources into JSON object representing all of the properties
+     * and objects of that resource.
+     *
+     * @param subjectResource
+     * @return JSON object representing the predicates and objects of that resource.
+     * @throws IOException
+     * @throws JSONException
+     */
     private static JSONObject processProperties(Resource subjectResource)
             throws IOException, JSONException {
         JSONObject out = new JSONObject();
 
         HashSet<Property> propertiesToProcess = new HashSet<Property>();
+        /* Iterate through all properties, and if it is used by this subject,
+         * add it to the set of properties to process. */
         StmtIterator allPropertiesIterator = subjectResource.listProperties();
         while (allPropertiesIterator.hasNext()) {
             Statement statement = allPropertiesIterator.nextStatement();
@@ -55,6 +117,8 @@ public class RDFToJSON {
                 propertiesToProcess.add(statement.getPredicate());
             }
         }
+        /* Iterate through properties found, convert each into a JSON Object
+         * and add result to an array, then attach that to the parent object. */
         for (Property property : propertiesToProcess) {
             String key = property.getURI();
             StmtIterator propertyIterator = subjectResource.listProperties(property);
@@ -69,6 +133,14 @@ public class RDFToJSON {
         return out;
     }
 
+    /**
+     * Convert individual statements into JSON representations of their object.
+     *
+     * @param property
+     * @return RDF/JSON object component.
+     * @throws IOException
+     * @throws JSONException
+     */
     private static JSONObject processProperty(Statement property) throws IOException, JSONException {
         JSONObject out = new JSONObject();
         if (property.getObject().isURIResource()) {

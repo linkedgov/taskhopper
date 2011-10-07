fbfdec66648ad6442bf3fc9da759e6e2016d9608
Tom Morris <tom@tommorris.org>
2011-10-07 12:38:26 +0100

    comments for RDFToXOM class

--- src/main/java/org/linkedgov/taskhopper/support/RDFToXOM.java
+++ src/main/java/org/linkedgov/taskhopper/support/RDFToXOM.java
@@ -6,7 +6,18 @@ import java.io.ByteArrayOutputStream;
 import nu.xom.Builder;
 import nu.xom.Document;
 
+/**
+ * Convenience class for turning Jena models into RDF/XML Documents with XOM.
+ *
+ * @author tom
+ */
 public class RDFToXOM {
+    /**
+     * Converts Jena model into RDF/XML document.
+     *
+     * @param model
+     * @return RDF/XML document as XOM <code>Document</code>.
+     */
     public static Document convertToXOM(Model model) {
         try {
             ByteArrayOutputStream rdfOutStream = new ByteArrayOutputStream();
